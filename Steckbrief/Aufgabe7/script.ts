59e3dabd54ec9b2bf74c2e09f4d7988e4612c426
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-01 12:36:12 +0100

    .

---
+++ Steckbrief/Aufgabe7/script.ts
@@ -0,0 +1,11 @@
+var nameFriends:string[]=['Seeli', 'Eisbär', 'Ele'];
+console.log(nameFriends.length);
+nameFriends.pop()
+
+console.log(nameFriends.length);
+
+console.log (nameFriends[0]);
+console.log (nameFriends[1]);
+
+nameFriends.push('Leon');
+console.log(nameFriends.length);
