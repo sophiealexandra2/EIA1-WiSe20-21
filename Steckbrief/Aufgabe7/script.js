3834d7c8a356aa38574154859bab32bc33c02f11
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-02 15:39:22 +0100

    .

--- Steckbrief/Aufgabe7/script.js
+++ Steckbrief/Aufgabe7/script.js
@@ -4,8 +4,7 @@ function changeBackground(color) {
 }
 window.addEventListener("load", function () { changeBackground('lightblue'); });
 //Sounds//
-var drumpad = ;
-;
+var drumpad = [];
 drumpad[0] = new Audio("./assets/snare.mp3");
 drumpad[1] = new Audio("./assets/G.mp3");
 drumpad[2] = new Audio("./assets/F.mp3");
@@ -15,4 +14,42 @@ drumpad[5] = new Audio("./assets/kick.mp3");
 drumpad[6] = new Audio("./assets/hihat.mp3");
 drumpad[7] = new Audio("./assets/laugh-1.mp3");
 drumpad[8] = new Audio("./assets/laugh-2.mp3");
+document.querySelector(".drumpad1").addEventListener("click", function () {
+    playSample(0);
+});
+document.querySelector(".drumpad2").addEventListener("click", function () {
+    playSample(1);
+});
+document.querySelector(".drumpad3").addEventListener("click", function () {
+    playSample(2);
+});
+document.querySelector(".drumpad4").addEventListener("click", function () {
+    playSample(3);
+});
+document.querySelector(".drumpad5").addEventListener("click", function () {
+    playSample(4);
+});
+document.querySelector(".drumpad6").addEventListener("click", function () {
+    playSample(5);
+});
+document.querySelector(".drumpad7").addEventListener("click", function () {
+    playSample(6);
+});
+document.querySelector(".drumpad8").addEventListener("click", function () {
+    playSample(7);
+});
+document.querySelector(".drumpad9").addEventListener("click", function () {
+    playSample(8);
+});
+
+document.querySelector(".playbutton").addEventListener("click", function(){
+    setInterval(function() {
+        playButton();
+    }, 500);
+    });
+
+function playSample(i) {
+    drumpad[i].play();
+}
+;
 //# sourceMappingURL=script.js.map
--- Steckbrief/Aufgabe7/script.ts
+++ Steckbrief/Aufgabe7/script.ts
@@ -7,7 +7,7 @@
  window.addEventListener("load",function() { changeBackground('lightblue') });
 
 //Sounds//
-var drumpad = ;;
+var drumpad = [];
 drumpad[0] = new Audio("./assets/snare.mp3");
 drumpad[1] = new Audio("./assets/G.mp3");
 drumpad[2] = new Audio("./assets/F.mp3");
@@ -18,3 +18,44 @@ drumpad[6] = new Audio("./assets/hihat.mp3");
 drumpad[7] = new Audio("./assets/laugh-1.mp3");
 drumpad[8] = new Audio("./assets/laugh-2.mp3");
 
+
+document.querySelector(".drumpad1").addEventListener("click", function(){
+    playSample(0);
+    });
+
+document.querySelector(".drumpad2").addEventListener("click", function(){
+    playSample(1);
+    });
+
+document.querySelector(".drumpad3").addEventListener("click", function(){
+    playSample(2);
+    });
+    
+document.querySelector(".drumpad4").addEventListener("click", function(){
+    playSample(3);
+    });
+
+document.querySelector(".drumpad5").addEventListener("click", function(){
+    playSample(4);
+    });
+
+document.querySelector(".drumpad6").addEventListener("click", function(){
+    playSample(5);
+    });
+
+document.querySelector(".drumpad7").addEventListener("click", function(){
+    playSample(6);
+    });
+
+document.querySelector(".drumpad8").addEventListener("click", function(){
+    playSample(7);
+    });
+
+document.querySelector(".drumpad9").addEventListener("click", function(){
+    playSample(8);
+    });
+
+
+    function playSample(i :number){
+        drumpad[i].play();
+    };
