e937b76c36722607b65b0c77e2f711b764e02147
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-02 15:24:24 +0100

    Audio added & css

--- Steckbrief/Aufgabe7/script.js
+++ Steckbrief/Aufgabe7/script.js
@@ -3,4 +3,16 @@ function changeBackground(color) {
     document.body.style.background = color;
 }
 window.addEventListener("load", function () { changeBackground('lightblue'); });
+//Sounds//
+var drumpad = ;
+;
+drumpad[0] = new Audio("./assets/snare.mp3");
+drumpad[1] = new Audio("./assets/G.mp3");
+drumpad[2] = new Audio("./assets/F.mp3");
+drumpad[3] = new Audio("./assets/C.mp3");
+drumpad[4] = new Audio("./assets/A.mp3");
+drumpad[5] = new Audio("./assets/kick.mp3");
+drumpad[6] = new Audio("./assets/hihat.mp3");
+drumpad[7] = new Audio("./assets/laugh-1.mp3");
+drumpad[8] = new Audio("./assets/laugh-2.mp3");
 //# sourceMappingURL=script.js.map
