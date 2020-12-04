325d964a911e7917ba31270cd1948e7c2c7daf17
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-04 17:08:15 +0100

    .

--- Steckbrief/Aufgabe7/script.ts
+++ Steckbrief/Aufgabe7/script.ts
@@ -18,7 +18,29 @@ drumpad[6] = new Audio("./assets/hihat.mp3");
 drumpad[7] = new Audio("./assets/laugh-1.mp3");
 drumpad[8] = new Audio("./assets/laugh-2.mp3");
 
+//Intervall//
+document.querySelector(".playbutton").addEventListener("click", function(){
+    setInterval(function() {
+        playbutton();
+    }, 500);
+    });
 
+// Funktion  für PlayButton
+
+function playbutton(){
+    
+    drumpad[2].play();
+    drumpad[3].play();
+    drumpad[4].play();
+
+};
+
+function playSample(i :number){
+    drumpad[i].play();
+};
+
+
+//einzelne buttons//
 document.querySelector(".drumpad1").addEventListener("click", function(){
     playSample(0);
     });
@@ -54,8 +76,3 @@ document.querySelector(".drumpad8").addEventListener("click", function(){
 document.querySelector(".drumpad9").addEventListener("click", function(){
     playSample(8);
     });
-
-
-    function playSample(i :number){
-        drumpad[i].play();
-    };
