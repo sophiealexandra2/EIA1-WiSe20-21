5392661822e5ac76647640cfdfaecbe902a7019e
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-09 15:33:53 +0100

    .

--- Steckbrief/Aufgabe8/script1.ts
+++ Steckbrief/Aufgabe8/script1.ts
@@ -16,38 +16,47 @@ drumpad[8] = new Audio("./assets/laugh-2.mp3");
 //einzelne buttons//
 document.querySelector(".drumpad1").addEventListener("click", function() {
     playSample(0);
+    recbeat(drumpad[0]);
     });
 
 document.querySelector(".drumpad2").addEventListener("click", function() {
     playSample(1);
+    recbeat(drumpad[1]);
     });
 
 document.querySelector(".drumpad3").addEventListener("click", function() {
     playSample(2);
+    recbeat(drumpad[2]);
     });
     
 document.querySelector(".drumpad4").addEventListener("click", function() {
     playSample(3);
+    recbeat(drumpad[3]);
     });
 
 document.querySelector(".drumpad5").addEventListener("click", function() {
     playSample(4);
+    recbeat(drumpad[4]);
     });
 
 document.querySelector(".drumpad6").addEventListener("click", function() {
     playSample(5);
+    recbeat(drumpad[5]);
     });
 
 document.querySelector(".drumpad7").addEventListener("click", function() {
     playSample(6);
+    recbeat(drumpad[6]);
     });
 
 document.querySelector(".drumpad8").addEventListener("click", function() {
-    playSample(7);
+    playSample(drumpad[7]);
+    recbeat(drumpad[7]);
     });
 
 document.querySelector(".drumpad9").addEventListener("click", function() {
-    playSample(8);
+    playSample(drumpad[8]);
+    recbeat(drumpad[8]);
     });
 
 // Funktion  für PlayButton ÄNDERN, playbutton wurde gelöscht
