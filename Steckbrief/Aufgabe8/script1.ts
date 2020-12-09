bb0900b52e2167660747b7c90f9f5c6231a03873
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-09 15:45:19 +0100

    Update script1.ts

--- Steckbrief/Aufgabe8/script1.ts
+++ Steckbrief/Aufgabe8/script1.ts
@@ -14,50 +14,54 @@ drumpad[8] = new Audio("./assets/laugh-2.mp3");
 
 
 //einzelne buttons//
-document.querySelector(".drumpad1").addEventListener("click", function() {
+document.querySelector(".drumpad1").addEventListener("click", () =>  {
     playSample(0);
     recbeat(drumpad[0]);
     });
 
-document.querySelector(".drumpad2").addEventListener("click", function() {
+document.querySelector(".drumpad2").addEventListener("click", () =>  {
     playSample(1);
     recbeat(drumpad[1]);
     });
 
-document.querySelector(".drumpad3").addEventListener("click", function() {
+document.querySelector(".drumpad3").addEventListener("click", () =>  {
     playSample(2);
     recbeat(drumpad[2]);
     });
     
-document.querySelector(".drumpad4").addEventListener("click", function() {
+document.querySelector(".drumpad4").addEventListener("click", () =>  {
     playSample(3);
     recbeat(drumpad[3]);
     });
 
-document.querySelector(".drumpad5").addEventListener("click", function() {
+document.querySelector(".drumpad5").addEventListener("click", () =>  {
     playSample(4);
     recbeat(drumpad[4]);
     });
 
-document.querySelector(".drumpad6").addEventListener("click", function() {
+document.querySelector(".drumpad6").addEventListener("click", () =>  {
     playSample(5);
     recbeat(drumpad[5]);
     });
 
-document.querySelector(".drumpad7").addEventListener("click", function() {
+document.querySelector(".drumpad7").addEventListener("click", () =>  {
     playSample(6);
     recbeat(drumpad[6]);
     });
 
-document.querySelector(".drumpad8").addEventListener("click", function() {
+document.querySelector(".drumpad8").addEventListener("click", () =>  {
     playSample(drumpad[7]);
     recbeat(drumpad[7]);
     });
 
-document.querySelector(".drumpad9").addEventListener("click", function() {
+document.querySelector(".drumpad9").addEventListener("click", () => {
     playSample(drumpad[8]);
     recbeat(drumpad[8]);
     });
+
+
+let trash: HTMLElement = document.querySelector(".fa-trash-alt");
+let recbutton: HTMLElement = document.querySelector(".fa-microphone");
 
 // Funktion  für PlayButton ÄNDERN, playbutton wurde gelöscht
 document.querySelector(".buttons").addEventListener("click", function() {
