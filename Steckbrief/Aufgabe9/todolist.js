6e77f80add64aba066ced2321a7655e2a09d267b
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2020-12-18 22:12:01 +0100

    .

--- Steckbrief/Aufgabe9/todolist.js
+++ Steckbrief/Aufgabe9/todolist.js
@@ -1,48 +1,49 @@
-var trashIcon = document.querySelector(".fas fa-trash-alt");
-var newTask = document.querySelector("#addNewTasks");
-var liElement = document.querySelector(".element");
-var listIndex = 0;
-function anzahl() {
-    document.querySelector("h2").innerHTML = listIndex + " in total";
-}
-function createElement() {
-    var myLi = document.createElement("li");
-    myLi.className = "element";
-    var trashIcon = document.createElement("i");
-    trashIcon.className = "fas fa-trash-alt";
-    var textInput = document.createElement("span");
-    textInput.innerHTML = newTask.value;
-    var circleIcon = document.createElement("i");
-    circleIcon.className = "fal fa-circle";
-    var checkedIcon = document.createElement("i");
-    checkedIcon.className = "fas fa-check hidden";
-    document.querySelector("#myList").appendChild(myLi);
-    myLi.appendChild(trashIcon);
-    myLi.appendChild(textInput);
-    myLi.appendChild(circleIcon);
-    myLi.appendChild(checkedIcon);
-    document.querySelector(".add").addEventListener("click", function () {
-        createElement();
-    });
-    circleIcon.addEventListener("click", function () {
-        if (checkedIcon.classList.contains("hidden")) {
-            checkedIcon.classList.remove("hidden");
+var Aufgabe9;
+(function (Aufgabe9) {
+    var trash = document.querySelector(".fas fa-trash-alt");
+    var newTask = document.querySelector("#addNewTasks");
+    var liElement = document.querySelector(".element");
+    var listIndex = 0;
+    function createElement() {
+        var myLi = document.createElement("li");
+        myLi.className = "element";
+        var trash = document.createElement("i");
+        trash.className = "fas fa-trash-alt";
+        var textInput = document.createElement("span");
+        textInput.innerHTML = newTask.value;
+        var circleIcon = document.createElement("i");
+        circleIcon.className = "fal fa-circle";
+        var doneIcon = document.createElement("i");
+        doneIcon.className = "fas fa-check hidden";
+        document.querySelector("#myList").appendChild(myLi);
+        myLi.appendChild(trash);
+        myLi.appendChild(textInput);
+        myLi.appendChild(circleIcon);
+        myLi.appendChild(doneIcon);
+        function amount() {
+            document.querySelector("h2").innerHTML = listIndex + " in total";
         }
-        else {
-            checkedIcon.classList.add("hidden");
-        }
-    });
-    trashIcon.addEventListener("click", function () {
-        document.querySelector("#myList").removeChild(myLi);
-        listIndex--;
-        anzahl();
-    });
-    listIndex++;
-    anzahl();
-}
-newTask.addEventListener("keypress", function (e) {
-    if (e.code === "Enter") {
-        createElement();
+        circleIcon.addEventListener("click", function () {
+            if (doneIcon.classList.contains("hidden")) {
+                doneIcon.classList.remove("hidden");
+            }
+            else {
+                doneIcon.classList.add("hidden");
+            }
+        });
+        trash.addEventListener("click", function () {
+            document.querySelector("#myList").removeChild(myLi);
+            listIndex--;
+            amount();
+        });
+        listIndex++;
+        amount();
     }
-});
+    newTask.addEventListener("keypress", function (event) {
+        if (event.key == "Enter") {
+            createElement();
+            newTask.value = " ";
+        }
+    });
+})(Aufgabe9 || (Aufgabe9 = {}));
 //# sourceMappingURL=todolist.js.map
--- Steckbrief/Aufgabe9/todolist.ts
+++ Steckbrief/Aufgabe9/todolist.ts
@@ -1,67 +1,61 @@
-const trashIcon: HTMLElement = document.querySelector(".fas fa-trash-alt");
+namespace Aufgabe9 {
+const trash: HTMLElement = document.querySelector(".fas fa-trash-alt");
 const newTask: any = document.querySelector("#addNewTasks");
 const liElement: HTMLElement = document.querySelector(".element");
+    
 let listIndex: number = 0;
-
-function anzahl(): void {
-    document.querySelector("h2").innerHTML = listIndex + " in total";
-}
 
 function createElement (): void {
     let myLi: HTMLElement = document.createElement("li");
     myLi.className = "element";
-    let trashIcon: HTMLElement = document.createElement("i");
-    trashIcon.className = "fas fa-trash-alt";
+    let trash: HTMLElement = document.createElement("i");
+    trash.className = "fas fa-trash-alt";
     let textInput: HTMLSpanElement = document.createElement("span");
     textInput.innerHTML = newTask.value;
     let circleIcon: HTMLElement = document.createElement("i");
     circleIcon.className = "fal fa-circle";
-    let checkedIcon: HTMLElement = document.createElement("i");
-    checkedIcon.className = "fas fa-check hidden";
-    
-
+    let doneIcon: HTMLElement = document.createElement("i");
+    doneIcon.className = "fas fa-check hidden";
     
     document.querySelector("#myList").appendChild(myLi);
-    myLi.appendChild(trashIcon);
+    myLi.appendChild(trash);
     myLi.appendChild(textInput);
     myLi.appendChild(circleIcon);
-    myLi.appendChild(checkedIcon);
+    myLi.appendChild(doneIcon);
+    
+    function amount(): void {
+        document.querySelector("h2").innerHTML = listIndex + " in total";
+    }
     
 
-    document.querySelector(".add").addEventListener("click", function(): void {
-        createElement();
-    });
-
-
-
     circleIcon.addEventListener("click", function(): void {
-        if (checkedIcon.classList.contains("hidden")) {
-            checkedIcon.classList.remove("hidden");
+        if (doneIcon.classList.contains("hidden")) {
+            doneIcon.classList.remove("hidden");
         }   
         else { 
-             checkedIcon.classList.add("hidden");
+             doneIcon.classList.add("hidden");
 
             }
    
     });
 
-    trashIcon.addEventListener("click", function(): void {
+    trash.addEventListener("click", function(): void {
         document.querySelector("#myList").removeChild(myLi);
         listIndex--;
-        anzahl();
+        amount();
     });
 
     listIndex++;
-    anzahl();
+    amount();
 
 }
 
-
-newTask.addEventListener("keypress", (e) => {
-    if (e.code === "Enter") {
+newTask.addEventListener("keypress", function (event: KeyboardEvent): void {
+    if (event.key == "Enter") {
         createElement();
+        newTask.value = " ";
     }
 
 });
 
-
+}
