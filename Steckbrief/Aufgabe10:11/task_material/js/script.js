be6afff619182c778a677744de4dfc3a2dca73fb
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-01-16 15:52:13 +0100

    .

--- Steckbrief/Aufgabe10:11/task_material/js/script.js
+++ Steckbrief/Aufgabe10:11/task_material/js/script.js
@@ -1,7 +1,7 @@
 var todosArray = [
     {
         text: "Task1",
-        checked: true
+        checked: false
     },
     {
         text: "Task2",
@@ -16,7 +16,18 @@ var inputDOMElement;
 var addButtonDOMElement;
 var todosDOMElement;
 var counterDOMElement;
+/**
+   Zwei neue hinzugefügt+ArtyomOn
+    */
+var OffenDOMElement;
+var DoneDOMElement;
+var artyomOn = false;
 window.addEventListener("load", function () {
+    /**
+    Zwei neue hinzugefügt
+     */
+    OffenDOMElement = document.querySelector("#Offen");
+    DoneDOMElement = document.querySelector("#Done");
     inputDOMElement = document.querySelector("#inputTodo");
     addButtonDOMElement = document.querySelector("#addButton");
     todosDOMElement = document.querySelector("#todos");
@@ -45,16 +56,21 @@ function drawListToDOM() {
     }
     updateCounter();
 }
+var Done = 1;
+var OffenerTask = 2;
 function updateCounter() {
     counterDOMElement.innerHTML = todosArray.length + " in total";
+    OffenDOMElement.innerHTML = OffenerTask + " tasks open";
+    DoneDOMElement.innerHTML = Done + " tasks done";
 }
-function addTodo() {
+function addTodo(text) {
     if (inputDOMElement.value != "") {
         todosArray.unshift({
             text: inputDOMElement.value,
             checked: false
         });
         inputDOMElement.value = "";
+        OffenerTask++;
         drawListToDOM();
     }
 }
@@ -66,4 +82,44 @@ function deleteTodo(index) {
     todosArray.splice(index, 1);
     drawListToDOM();
 }
+//Artyom:
+window.addEventListener("load", function () {
+    var artyom = new Artyom();
+    artyom.addCommands({
+        smart: true,
+        indexes: ["Erstelle Aufgabe *"],
+        action: function (i, wildcard) {
+            artyomOn = true;
+            addTodo(wildcard);
+        }
+    });
+    document.querySelector("#start-voice").addEventListener("click", function () {
+        artyom.dontObey();
+        artyom.initialize({
+            lang: "de-DE"
+        });
+        artyom.say("Hey Hey! Sag mir, was du heute erledigen willst.");
+        setTimeout(function () {
+            artyom.obey();
+        }, 8000);
+        startArtyom();
+    });
+    document.querySelector("#stop-voice").addEventListener("click", function () {
+        stopArtyom();
+        artyom.say("See ya");
+    });
+    function startArtyom() {
+        artyom.initialize({
+            lang: "de-DE",
+            continuous: true,
+            listen: true,
+            interimResults: true,
+            debug: true,
+            speed: 1
+        });
+    }
+    function stopArtyom() {
+        artyom.fatality();
+    }
+});
 //# sourceMappingURL=script.js.map
--- Steckbrief/Aufgabe10:11/task_material/js/script.ts
+++ Steckbrief/Aufgabe10:11/task_material/js/script.ts
@@ -23,8 +23,22 @@ var inputDOMElement: HTMLInputElement;
 var addButtonDOMElement: HTMLElement;
 var todosDOMElement: HTMLElement;
 var counterDOMElement: HTMLElement;
+ /**
+    Zwei neue hinzugefügt+ArtyomOn
+     */
+var OffenDOMElement: HTMLElement;
+var DoneDOMElement: HTMLElement;
+var artyomOn: boolean = false;
+
+
 
 window.addEventListener("load", function(): void {
+    /**
+    Zwei neue hinzugefügt
+     */
+    OffenDOMElement = document.querySelector("#Offen");
+    DoneDOMElement = document.querySelector("#Done");
+
     inputDOMElement = document.querySelector("#inputTodo");
     addButtonDOMElement = document.querySelector("#addButton");
     todosDOMElement = document.querySelector("#todos");
@@ -53,17 +67,23 @@ function drawListToDOM(): void {
 
     updateCounter();
 }
+var Done: number = 1;
+var OffenerTask: number = 2;
 
 function updateCounter(): void {
     counterDOMElement.innerHTML = todosArray.length + " in total";
+    OffenDOMElement.innerHTML = OffenerTask + " tasks open";
+    DoneDOMElement.innerHTML = Done + " tasks done";
 }
-function addTodo(): void {
+
+function addTodo(text: string): void {
     if (inputDOMElement.value != "") {
         todosArray.unshift({
             text: inputDOMElement.value,
             checked: false
         });
         inputDOMElement.value = "";
+        OffenerTask ++;
         drawListToDOM();
     }
 }
@@ -77,3 +97,46 @@ function deleteTodo(index: number): void {
     drawListToDOM();
 }
 
+//Artyom:
+
+window.addEventListener("load", function(): void {
+    const artyom: any = new Artyom();
+    artyom.addCommands({
+        smart: true,
+        indexes: ["Erstelle Aufgabe *"],
+        action: function (i: any, wildcard: string): void {
+            artyomOn = true;
+            addTodo(wildcard);
+        }
+    });
+
+    document.querySelector("#start-voice").addEventListener("click", function(): void {
+        artyom.dontObey();
+        artyom.initialize({
+            lang: "de-DE"
+        });
+        artyom.say("Hey Hey! Sag mir, was du heute erledigen willst.");
+        setTimeout(function (): void {
+            artyom.obey();
+        }, 8000);
+        startArtyom();
+    });
+    document.querySelector("#stop-voice").addEventListener("click", function(): void {
+        stopArtyom();
+        artyom.say("See ya");
+    });
+
+    function startArtyom(): void {
+        artyom.initialize({
+                lang: "de-DE",
+                continuous: true,
+                listen: true,
+                interimResults: true,
+                debug: true,
+                speed: 1
+        });
+    }
+    function stopArtyom(): void {
+        artyom.fatality();
+    }
+});
